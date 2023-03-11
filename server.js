5bcfa0769f36337689e83315b82218b86e2c81d2
Op Pautu <pautuop@gmail.com>
2023-03-11 21:40:56 +0530

    Style form

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const router = require("./routes/index");
 const ejs = require("ejs");
 const app = express();
 const bodyParser = require("body-parser");
-
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
-app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
